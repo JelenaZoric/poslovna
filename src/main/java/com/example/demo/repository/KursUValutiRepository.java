d1c4f4e3cbb35fd7bb55f4de3d20468d741bde0f
vjelena <vulinova78@gmail.com>
2018-07-11 20:48:43 +0200

    kursevi u valuti crud

---
+++ src/main/java/com/example/demo/repository/KursUValutiRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.model.KursUValuti;
+
+public interface KursUValutiRepository extends JpaRepository<KursUValuti, Long>{
+
+}
